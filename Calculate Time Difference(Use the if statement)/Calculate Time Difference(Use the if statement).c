c6b691feca49dbb4084267f330d51b96a1882590
BadBoy1314love <2574715537@qq.com>
2019-02-28 09:51:48 +0800

    *

--- Calculate Time Difference(Use the if statement)/Calculate Time Difference(Use the if statement).c
+++ Calculate Time Difference(Use the if statement)/Calculate Time Difference(Use the if statement).c
@@ -5,19 +5,21 @@ int main()
 	int hour1, minute1;
 	int hour2, minute2;
 
-	printf("请输入起始时间：");
+	printf("Please enter the start time:");
 	scanf_s("%d %d", &hour1, &minute1);
-	printf("请输入终止时间：");
+	printf("Please enter the termination time:");
 	scanf_s("%d %d", &hour2, &minute2);
 
 	int Hour = hour2 - hour1;
 	int Minute = minute2 - minute1;
 
-	if (Minute < 0)     //如果（Minute<0，那么则执行大括号内语句）
+	if (Minute < 0)     //If (Minute<0, then execute the statement inside the braces)
 	{
 		Minute = 60 + Minute;
 		Hour--;
 	}
 
-	printf("则时间差是%d小时%d分", Hour, Minute);
+	printf("Then the time difference is %d hours %d minutes", Hour, Minute);
 }
+
+
