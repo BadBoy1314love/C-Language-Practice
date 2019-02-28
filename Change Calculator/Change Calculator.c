663e670c0c1d6fb955d6fc92be6f5e66e0ac895f
BadBoy1314love <2574715537@qq.com>
2019-02-28 11:00:05 +0800

    Change Calculator

--- Change Calculator/Change Calculator.c
+++ Change Calculator/Change Calculator.c
@@ -5,18 +5,18 @@ int main()
 	int Price = 0;
 	int Bill = 0;
 
-	printf("请输入商品金额：");
+	printf("Please enter the item amount：");
 	scanf_s("%d", &Price);
-	printf("请输入票面：");
+	printf("Please enter the bill：");
 	scanf_s("%d", &Bill);
 
 	if (Bill >= Price)
 	{
-		printf("应该找零%d元", Bill - Price);
+		printf("The change of %d yuan", Bill - Price);
 	}
 	else
 	{
-		printf("你的钱不够,还差%d元",Price-Bill);
+		printf("You don't have enough money. You still need %d yuan",Price-Bill);
 	}
 
 	return 0;
