1f4882fc7b3e38ee10f395adcc806acd1c0f3632
BadBoy1314love <2574715537@qq.com>
2019-02-28 09:15:29 +0800

    Calculate Average

--- Calculate Average/Calculate Average.c
+++ Calculate Average/Calculate Average.c
@@ -4,10 +4,10 @@ int main()
 {
 	int a, b;
 
-	printf("请输入两个整数\n");
+	printf("Please enter two integers\n");
 	scanf_s("%d %d", &a, &b);
 
 	double c = (a + b) / 2.0;
 
-	printf("%d与%d的平均值=%f\n", a, b, c);
+	printf("The average of %d and %d = %f \n", a, b, c);
 }
