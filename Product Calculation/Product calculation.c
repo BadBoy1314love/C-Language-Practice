32c7ee5febd3819ccaabd340ea94b9a21e5327c9
BadBoy1314love <2574715537@qq.com>
2019-02-28 11:18:52 +0800

    Product Calculation

--- Product Calculation/Product calculation.c
+++ Product Calculation/Product calculation.c
@@ -5,12 +5,13 @@ int main()
 	const int AMOUNT = 100;
 	int price = 0;
 
-	printf("请输入金额（元）：");
+	printf("Please enter the amount (yuan)：");
 	scanf_s("%d", &price);
 
 	int change = AMOUNT - price;
 
-	printf("找您%d元。\n", change);
+	printf("Change %d yuan.\n", change);
 
 	return 0;
 }
+
