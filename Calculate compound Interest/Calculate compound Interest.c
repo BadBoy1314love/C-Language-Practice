5b949e8f44e381bcc09e4dca49959284461bba09
BadBoy1314love <2574715537@qq.com>
2019-02-28 09:28:12 +0800

    Calculate compound Interest

--- Calculate compound Interest/Calculate compound Interest.c
+++ Calculate compound Interest/Calculate compound Interest.c
@@ -4,10 +4,10 @@ int main()
 {
 	int x;
 
-	printf("请输入定期金额\n");
+	printf("Please enter a regular amount\n");
 	scanf_s("%d", &x);
 
 	double amount = x * (1 + 0.033) * (1 + 0.033) * (1 + 0.033);
 
-	printf("本息合计%f", amount);
+	printf("Total principal and interest %f yuan", amount);
 }
