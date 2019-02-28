38abfa1a1bdf76a33d538df67a460632f9906d61
BadBoy1314love <2574715537@qq.com>
2019-02-28 08:41:02 +0800

    BCD Decryption

--- BCD Decryption/BCD Decryption.c
+++ BCD Decryption/BCD Decryption.c
@@ -1,10 +1,11 @@
 /*
 A BCD number is a two-digit decimal number in one byte,
 with four bits for each digit.So if the hexadecimal of a BCD number is 0x12,
-it represents decimal 12. But xiao Ming has not learned BCD,
+it represents decimal 12. But XiaoMing has not learned BCD,
 so all BCD Numbers are converted to decimal output as binary Numbers.
 So BCD 0x12 is written as decimal 18!Now, 
-your program will read in the wrong decimal number and output the correct decimal number.Tip: you can convert 18 back to 0x12 and then back to 12.
+your program will read in the wrong decimal number and output the correct
+decimal number.Tip: you can convert 18 back to 0x12 and then back to 12.
 */
 
 #include <stdio.h> 
