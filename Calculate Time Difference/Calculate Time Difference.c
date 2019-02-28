c5a8dc2cd5e04eef80093e5cc3b73080f50c6e6e
BadBoy1314love <2574715537@qq.com>
2019-02-28 10:51:13 +0800

    Calculate Time Difference

--- Calculate Time Difference/Calculate Time Difference.c
+++ Calculate Time Difference/Calculate Time Difference.c
@@ -5,14 +5,14 @@ int main()
 	int hour1, minute1;
 	int hour2, minute2;
 
-	printf("请输入起始时间(请使用24H制)\n");
+	printf("Please enter the start time (please use 24H system)\n");
 	scanf_s("%d %d",&hour1, &minute1);
-	printf("请输入终止时间(请使用24H制)\n");
+	printf("Please enter the termination time (please use 24H system)\n");
 	scanf_s("%d %d",&hour2, &minute2);
 
 	int t1 = hour1 * 60 + minute1;
 	int t2 = hour2 * 60 + minute2;
 	int t = t2 - t1;
 
-	printf("时间差是%d小时%d分钟", t / 60, t % 60);
+	printf("Time difference is %d hours %d minutes",t / 60, t % 60);
 }
