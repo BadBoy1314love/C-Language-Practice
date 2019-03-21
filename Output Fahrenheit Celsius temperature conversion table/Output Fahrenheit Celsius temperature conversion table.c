88a1c00235d3167dc79441e9d8fa65ed5750a439
BadBoy1314love <2574715537@qq.com>
2019-03-21 11:18:29 +0800

    Output Fahrenheit Celsius temperature conversion table

---
+++ Output Fahrenheit Celsius temperature conversion table/Output Fahrenheit Celsius temperature conversion table.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+	int LowerFahrenheit;
+	int UpperFahrenheit;
+	double C = 0;
+
+	printf("Please enter the lower Fahrenheit value:");
+	scanf_s("%d", &LowerFahrenheit);
+	
+	printf("Please enter the upper Fahrenheit value:");
+	scanf_s("%d", &UpperFahrenheit);
+
+	if ((LowerFahrenheit <= UpperFahrenheit)&& (UpperFahrenheit<= 100))
+	{
+		while ((LowerFahrenheit <= UpperFahrenheit) && (UpperFahrenheit <= 100))
+		{
+			printf("F=%d C=%.1lf\n",LowerFahrenheit,C=5*(LowerFahrenheit-32)/9);
+			LowerFahrenheit = LowerFahrenheit + 2;
+		}
+	}
+	else
+	{
+		printf("Invalid\n");
+	}
+}
