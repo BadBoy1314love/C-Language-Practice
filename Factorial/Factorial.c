c024df7d7eec8fca52dfe2fa9fcbce68a7b55cf7
BadBoy1314love <43374153+BadBoy1314love@users.noreply.github.com>
2019-04-11 11:02:54 +0800

    Factorial

--- Factorial/Factorial.c
+++ Factorial/Factorial.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 int main()
@@ -13,6 +14,51 @@ int main()
 	{
 		Factorial *= Initial;
 		Initial++;
+	}
+
+	printf("%d!=%d", Number, Factorial);
+
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+	int Number;
+	int Factorial=1;
+	int Initial = 1;
+
+	printf("please enter a number:");
+	scanf_s("%d", &Number);
+
+	for (Initial = 1; Initial <= Number; Initial++)
+	{
+		Factorial *= Initial;
+	}
+
+	printf("%d!=%d", Number, Factorial);
+
+	return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int Number;
+	int Factorial = 1;
+	int Initial = 1;
+
+	printf("Please enter a number:");
+	scanf_s("%d", &Number);
+
+	for (Initial=Number;Initial>1;Initial--)
+	{
+		Factorial *= Initial;
 	}
 
 	printf("%d!=%d", Number, Factorial);
