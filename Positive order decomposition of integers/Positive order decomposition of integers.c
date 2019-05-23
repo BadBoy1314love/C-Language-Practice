b46dd6256d2dc4a1a38932d77a951be5a731a803
BadBoy1314love <43374153+BadBoy1314love@users.noreply.github.com>
2019-05-23 22:57:45 +0800

    Positive order decomposition of integers

---
+++ Positive order decomposition of integers/Positive order decomposition of integers.c
@@ -0,0 +1,13 @@
+/*
+Positive order decomposition of integers
+Input a non-negative integer, and output its digits in positive order
+Input:12345
+Output:1 2 3 4 5
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+}
